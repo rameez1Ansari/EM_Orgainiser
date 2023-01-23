fb2b298bfad9ca4de58adab498995f1f6fa1c107
Rameez Ansari <you@example.com>
2023-01-23 14:08:44 +0530

    this is first commpleteion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidenavComponent } from '../app/include/sidenav/sidenav.component';
+import { NavbarComponent } from '../app/include/navbar/navbar.component';
+import { CalanderComponent } from './templates/calander/calander.component';
+import { TimeLineComponent } from './templates/time-line/time-line.component';
+import { SignupComponent } from './templates/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    NavbarComponent,
+    CalanderComponent,
+    TimeLineComponent,
+    SignupComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/templates/calander/calander.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {  ChangeDetectionStrategy } from '@angular/core';
+
+
+@Component({
+  selector: 'app-calander',
+  templateUrl: './calander.component.html',
+  styleUrls: ['./calander.component.css']
+})
+export class CalanderComponent {
+
+
+  
+
+}
+
+
